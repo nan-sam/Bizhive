9e3e932c6f73a7067e8574179d295b90edeb75d1
Nange Sam <nangesam1@gmail.com>
2025-03-08 22:35:17 -0500

    Fixed fetching issues

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,14 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 const serverPort = process.env.PORT || 3000;
-console.log(`api need to run on ${serverPort} for vite server`);
+console.log(`api needs to run on ${serverPort} for vite server`);
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': `http://localhost:${ serverPort || 3000}`
-    }
-  }
-})
+      "/api": `http://localhost:${serverPort || 3000}`,
+    },
+  },
+});
